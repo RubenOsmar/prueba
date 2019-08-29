97b3701580335d69466a313463b7772e83f0028b
/Ruben Osmar <ruben_osmar19@hotmail.com>
2019-08-28 20:20:37 -0500

    Modificación del archivo src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,2 @@
 import { Component } from '@angular/core';
-@Component({
-selector: 'app-root',
-templateUrl: './app.component.html',
-styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-title = 'Tour of Heroes';
-}
+@Component({ selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css'] }) export class AppComponent { title = 'Galeria de Heroes'; }
