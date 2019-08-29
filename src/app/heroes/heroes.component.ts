da6b33304ce22e88d28b9f97db05beb4d24f9427
/Ruben Osmar <ruben_osmar19@hotmail.com>
2019-08-28 20:26:57 -0500

    Modificamos el archivo /heroes.component. ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
-
-@Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
+import { Component, OnInit } from '@angular/core'; import { Hero } from '../hero'; import { HEROES } from '../mock-heroes';
+@Component({ selector: 'app-heroes', templateUrl: './heroes.component.html', styleUrls: ['./heroes.component.css'] })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'IronMan'
-    };
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+heroes = HEROES; selectedHero: Hero;
+constructor() { }
+ngOnInit() { }
+onSelect(hero: Hero): void { this.selectedHero = hero; }
 }
